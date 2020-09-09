91912a70b077293f88e623c27d23fd561cabb185
Alexhromov <grom-a@ukr.net>
2020-09-09 15:11:07 +0300

    Delete trash

--- zip/08_UIFragments/CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/Crime.kt
+++
@@ -1,7 +0,0 @@
-package com.bignerdranch.android.criminalintent
-
-
-data class Crime(val id: UUID = UUID.randomUUID(),
-                 var title: String = "",
-                 var date: Date = Date(),
-                 var isSolved: Boolean = false)
--- zip/19_MVVM/BeatBox/app/src/main/java/com/bignerdranch/android/beatbox/Sound.kt
+++
@@ -1,8 +0,0 @@
-package com.bignerdranch.android.beatbox
-
-private const val WAV = ".wav"
-
-class Sound(val assetPath: String) {
-
-    val name = assetPath.split("/").last().removeSuffix(WAV)
-}
